f7a48386a322fb6cc53a03a0584be23afc8b0855
Praveen Dilshan <rosapraveen1@gmail.com>
2024-10-07 02:20:33 +0530

    Modified

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
+
+
 const Home = () => {
   return (
     <div>
@@ -11,6 +13,7 @@ const Home = () => {
         <SpecialityMenu/>
         <TopDoctors/>
         <Banner/>
+        
     </div>
   )
 }
